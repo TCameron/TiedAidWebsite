beae19d91e01ac026366b34e1f307bfeae48b6be
Timothy Cameron <tcameron6887@gmail.com>
2018-09-20 16:43:31 -0400

    Added file

---
+++ public_html/src/_config/connect.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tcameron
+ * Date: 7/20/2016
+ * Time: 14:31
+ */
+
+$servername = "localhost";
+$username = "tied_aid";
+$password = "";
+$dbname = "tied_aid";
+
+// Create Connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check Connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
